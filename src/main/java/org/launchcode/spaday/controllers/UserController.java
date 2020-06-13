aef5e44eb2062993c1c328756fb003f8c345e52d
Elizabeth McMickle <emartchink@yahoo.com>
2020-06-13 12:00:30 -0500

    Added error handling

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -3,31 +3,42 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("username", user.getUserName());
-
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        Boolean hasError = false;
+        if (!user.getPassword().equals(verify)) {
+            model.addAttribute("passwordError", "Passwords must match!");
+            hasError = true;
         }
-        model.addAttribute("error", "Passwords must match!");
-        return "user/add";
+        if (errors.hasErrors()) {
+            hasError = true;
+        }
+        if (hasError) {
+            return "user/add";
+        }
 
+        return "user/index";
     }
 
 
